7fc8a20b62b73847b39ca4dc0aff8b057d1f015c
Jagadeeshbattu <115805937+Jagadeeshbattu@users.noreply.github.com>
2023-01-07 13:02:44 +0530

    Add files via upload
    Initial commit

---
+++ HelloWorld/src/com/jala/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.jala;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	System.out.println("AWS With DevOps");
+	System.out.println("AWS With DevOps");
+	System.out.println("AWS With DevOps");
+	System.out.println("AWS With DevOps");
+	
+	
+	}
+	
+
+}
